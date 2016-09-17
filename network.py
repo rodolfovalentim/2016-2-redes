510357efc530addb8dc46833eeca58dc622395a8
Rodolfo Valentim <rodolfovieiravalentim@gmail.com>
2016-09-17 10:43:39 -0300

    Adding keyboard input

--- network.py
+++ network.py
@@ -1,6 +1,32 @@
 from node import *
+import logging
 
-node = Node('192.168.1.100')
-node.join('192.168.1.142')
-#node.lookup(1, ('192.168.1.142', 12233))
-#node.update()
+# 192.168.1.235
+
+def join():
+    pass
+def leave():
+    pass
+def lookup():
+    pass
+def update():
+    pass
+
+def keyboard():
+    while True:
+        nb = raw_input('< JOIN / LEAVE / LOOKUP / UPDATE / EXIT > Choose:')
+        if (nb == 'JOIN'):
+            logging.info('node.join()')
+        elif (nb == 'LEAVE'):
+            logging.info('node.leave()')
+        elif (nb == 'LOOKUP'):
+            logging.info('node.lookup()')
+        elif (nb == 'UPDATE'):
+            logging.info('node.update()')
+        elif (nb == 'EXIT'):
+            break
+        else:
+            logging.error('Option not find')
+    logging.error('Exiting')
+
+keyboard()
