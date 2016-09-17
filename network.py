48a0cb595d436ffeee06e49d8fc4597c528d19d3
Rodolfo Valentim <rodolfovieiravalentim@gmail.com>
2016-09-17 16:15:58 -0300

    Beta #01

--- network.py
+++ network.py
@@ -3,49 +3,64 @@ import logging
 
 # 192.168.1.235
 node = None
-default_port = 12233
 
 def create():
-    pass
     node.create()
+    node.set_mask(0, True)
+    node.set_mask(1, True)
+    node.set_mask(2, True)
+    node.set_mask(3, True)
+
 def join():
     contact_ip = raw_input('IP of your contact in the network: ')
 
     while(True):
-        (, , ip_sucessor) = node.lookup(node.key, (contact_ip, default_port))
-        if(not node.join(ip_sucessor))
+        ip_sucessor = node.lookup(node.key, (contact_ip, node.port))
+        logging.info(ip_sucessor)
+        if(not node.join(ip_sucessor[2])):
             node.get_new_key()
             continue
-        if(not node.update())
+        if(not node.update()):
             node.get_new_key()
             continue
         break
+    node.set_mask(0, True)
+    node.set_mask(1, True)
+    node.set_mask(2, True)
+    node.set_mask(3, True)
 
 def leave():
-    pass
-    #leave
+    node.set_mask(0, False)
+    node.set_mask(1, False)
+    node.set_mask(2, False)
+    node.set_mask(3, False)
+    node.leave()
 def lookup():
-    pass
-    #recebe input
-    #lookup
+    key_input = raw_input('Type the key to look up: ')
+    ip_sucessor = node.lookup(key_input, (node.ip_next, node.port))
+    logging.info('Answer lookup %s' % ip_sucessor)
+
 def update():
-    pass
-    #lookup
+    node.update()
+
+def exit()
+    node.kill_listener()
 
 def keyboard():
     while True:
         nb = raw_input('< CREATE / JOIN / LEAVE / LOOKUP / UPDATE / EXIT > Choose:')
         if (nb == 'CREATE'):
-            logging.info('node.create()')
+            create()
         elif (nb == 'JOIN'):
-            logging.info('node.join()')
+            join()
         elif (nb == 'LEAVE'):
-            logging.info('node.leave()')
+            leave()
         elif (nb == 'LOOKUP'):
-            logging.info('node.lookup()')
+            lookup()
         elif (nb == 'UPDATE'):
-            logging.info('node.update()')
+            update()
         elif (nb == 'EXIT'):
+            exit()
             break
         else:
             logging.error('Option not find')
