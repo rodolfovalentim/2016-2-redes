8eba02daa63c5c3b4a20e69f8c643177b04649c9
Rodolfo Valentim <rodolfovieiravalentim@gmail.com>
2016-09-17 19:02:37 -0300

    Stable version #03

--- network.py
+++ network.py
@@ -35,6 +35,7 @@ def leave():
     node.set_mask(2, False)
     node.set_mask(3, False)
     node.leave()
+
 def lookup():
     key_input = raw_input('Type the key to look up: ')
     ip_sucessor = node.lookup(int(key_input), (node.ip_next, node.port))
@@ -53,27 +54,55 @@ def start_listener():
 def info():
     print (node)
 
-def start_keyboard():
+def list_nodes():
+    t = PrettyTable(['COUNT','IP', 'KEY'])
+    i = 0
+    key = node.key
+    ip = node.ip
     while True:
-        nb = raw_input('< CREATE / JOIN / LEAVE / LOOKUP / UPDATE / INFO / EXIT > Choose:')
-        if (nb == 'CREATE'):
-            create()
-        elif (nb == 'JOIN'):
-            join()
-        elif (nb == 'LEAVE'):
-            leave()
-        elif (nb == 'LOOKUP'):
-            lookup()
-        elif (nb == 'UPDATE'):
-            update()
-        elif (nb == 'INFO'):
-            info()
-        elif (nb == 'EXIT'):
-            exit()
+        info_sucessor = node.lookup(int(key), (ip, node.port))
+        t.add_row([str(i), ip, str(key)])
+        i = i + 1
+        key = info_sucessor[1]
+        ip = info_sucessor[2]
+        if (key == node.key):
             break
+    print t
+
+def start_keyboard():
+    state = 0
+    while True:
+        if (state == 0):
+            nb = raw_input('< CREATE / JOIN / INFO / EXIT > Choose:')
+            if (nb == 'CREATE'):
+                create()
+                state = 1
+            elif (nb == 'JOIN'):
+                join()
+                state = 1
+            elif (nb == 'INFO'):
+                info()
+            elif (nb == 'EXIT'):
+                exit()
+                break
+            else:
+                logging.warning('Option not find')
         else:
-            logging.error('Option not find')
-    logging.error('Exiting')
+            nb = raw_input('< LEAVE / LOOKUP / UPDATE / INFO / LIST > Choose:')
+            if (nb == 'LEAVE'):
+                leave()
+                state = 0
+            elif (nb == 'LOOKUP'):
+                lookup()
+            elif (nb == 'UPDATE'):
+                update()
+            elif (nb == 'INFO'):
+                info()
+            elif (nb == 'LIST'):
+                list_nodes()
+            else:
+                logging.warning('Option not find')
+    logging.info('Exiting')
 
 ip = raw_input('Your IP: ')
 node = Node(ip)
